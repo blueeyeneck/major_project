68466ea6dfeec59af8cf6ecf82eca9063ecfd82d
mahindra <d.mahindra04010611@gmail.com>
2023-10-16 00:56:18 +0530

    cookie demos

--- classroom/server.js
+++ classroom/server.js
@@ -2,8 +2,33 @@ const express = require('express');
 const app = express();
 const posts = require("./routes/posts.js");
 const users = require("./routes/users.js");
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser("secreatcode"));
+
+app.get("/getsignedcookie",(req,res)=>{
+    res.cookie("made in","india",{signed:true});
+    console.dir(req.signedCookies);
+    res.send("signed cookie is send");
+});
+
+app.get("/verify",(req,res)=>{
+    res.send(req.signedCookies);
+});
+
+app.get("/greet",(req,res)=>{
+    let {name = "anonymous"} = req.cookies;
+    res.send(`namasthe ${name}`);
+});
+
+app.get("/getcookies",(req,res)=>{
+    res.cookie("greet","namaste");
+    res.cookie("origin","indian");
+    res.send("send some cookies");
+});
 
 app.get("/",(req,res)=>{
+    console.dir(req.cookies);
     res.send('hi i am root');
 });
 
